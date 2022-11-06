54ed51f19d0b24da083a72e71d6d7c15e403bf89
Surender Soundiramourty <psm.surender@gmail.com>
2022-11-07 02:09:46 +0530

    added ajax for php request when registering

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+$(document).ready (function() {
+    sendRegistration()
+    
+});  
+
+
 function sendRegistration(){
-    var form = $
+    $('form[name="registration"]').on('submit', function(){
+        $(this).validate();
+        if(!$(this).valid()) return;
+        var ref = $(this),
+            method = ref.attr("method"),
+            url = ref.attr("action"),
+            data = {};
+        ref.find('[name]').each(function(index, value) {
+            var ref = $(this)
+                nm = ref.attr("name"),
+                value = ref.val();
+
+            data[nm] = value;
+        });
+
+        $.ajax({
+            url: url,
+            type: method,
+            data: data,
+            success: function(response) {
+                alert("Your waste has been successfully registered.");
+            }
+        });
+        return false;
+    });
 }
